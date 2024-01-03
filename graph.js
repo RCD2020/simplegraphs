39c1e4991ea433535e9871307470981a648433af
RCD2020 <dobertravis@icloud.com>
2024-01-02 18:18:24 -0700

    overhauled how vectors are stored

--- graph.js
+++ graph.js
@@ -1,3 +1,16 @@
+function Vector(movement, start, label, color='black') {
+    this.x1 = start[0];
+    this.y1 = start[1];
+    this.vecX = movement[0];
+    this.vecY = movement[1];
+    this.x2 = this.x1 + this.vecX;
+    this.y2 = this.y1 + this.vecY;
+    
+    this.label = label;
+    this.color = color;
+}
+
+
 function Graph(id) {
     this.canvas = document.getElementById(id);
     this.ctx = this.canvas.getContext('2d');
@@ -8,15 +21,10 @@ function Graph(id) {
     this.ySize = NaN;
 
     this.draw = function() {
-        // if size 0 in any dimension, then throw error
-        let xChange = this.xRange[1] - this.xRange[0];
-        let yChange = this.yRange[1] - this.yRange[0];
+        // if bounds not set, then throw error
         if (this.xRange.length == 0 || this.yRange.length == 0) {
             throw new Error('not enough data in graph');
         }
-
-        // get size of each square
-        let sizes = this.getUnitSize();
 
         // draw grid
         this.ctx.strokeStyle = 'gray';
@@ -24,69 +32,77 @@ function Graph(id) {
 
         this.ctx.beginPath();
         for (let i = this.xRange[0]; i < this.xRange[1] + 1; i++) {
-            this.ctx.moveTo(i * sizes[0], 0);
-            this.ctx.lineTo(i * sizes[0], this.canvas.height);
+            this.ctx.moveTo(i * this.xSize, 0);
+            this.ctx.lineTo(i * this.xSize, this.canvas.height);
             this.ctx.stroke();
         }
 
         for (let i = this.yRange[0]; i < this.yRange[1] + 1; i++) {
-            this.ctx.moveTo(0, i * sizes[1]);
-            this.ctx.lineTo(this.canvas.width, i * sizes[1]);
+            this.ctx.moveTo(0, i * this.ySize);
+            this.ctx.lineTo(this.canvas.width, i * this.ySize);
             this.ctx.stroke();
         }
         this.ctx.closePath();
 
         // draw vectors
-        this.ctx.strokeStyle = 'black';
-        this.ctx.lineWidth = 2;
+        // this.ctx.strokeStyle = 'black';
+        // this.ctx.lineWidth = 2;
 
-        this.ctx.beginPath();
-        for (let i = 0; i < this.vectors.length; i++) {
-            let x = this.vectors[i][2][0];
-            let y = this.vectors[i][2][1];
+        // this.ctx.beginPath();
+        // for (let i = 0; i < this.vectors.length; i++) {
+        //     let x1 = this.getDrawCoord(this.vectors[i][2][0]);
+        //     let x2 = this.getDrawCoord(this.vectors[i][2][0] + );
+        //     let y1 = this.getDrawCoord(this.vectors[i][2][1], false);
 
-            this.ctx.moveTo(
-                x * sizes[0], (y + this.vectors[i][1]) * sizes[1]);
-            this.ctx.lineTo(
-                (x + this.vectors[i][0]) * sizes[0],
-                y * sizes[1]
-            );
-            this.ctx.stroke();
-        }
-        this.ctx.closePath();
+        //     this.ctx.moveTo(
+        //         x * sizes[0], (y + this.vectors[i][1]) * sizes[1]);
+        //     this.ctx.lineTo(
+        //         (x + this.vectors[i][0]) * sizes[0],
+        //         y * sizes[1]
+        //     );
+        //     this.ctx.stroke();
+        // }
+        // this.ctx.closePath();
 
         return 1;
     }
 
-    this.addVector = function(x, y, start, label) {
-        let xVec = [start[0], start[0] + x];
-        let yVec = [start[1], start[1] + y];
-        console.log(xVec);
-        console.log(yVec);
+    this.addVector = function(x, y, vecX, vecY, label, color='black') {
+        let vector = new Vector([vecX, vecY], [x, y], label, color);
 
+        this.pushVector(vector);
+    }
+
+    this.pushVector = function(vector) {
+        this.increaseBounds(
+            vector.x1, vector.y1,
+            vector.x2, vector.y2
+        );
+
+        this.vectors.push(vector)
+    }
+
+    this.increaseBounds = function(x1, y1, x2, y2) {
         if (this.xRange.length == 0) {
-            this.xRange.push(Math.min(...xVec));
-            this.xRange.push(Math.max(...xVec));
-            this.yRange.push(Math.min(...yVec));
-            this.yRange.push(Math.max(...yVec));
+            this.xRange.push(Math.min(x1, x2));
+            this.xRange.push(Math.max(x1, x2));
+            this.yRange.push(Math.min(y1, y2));
+            this.yRange.push(Math.max(y1, y2));
         } else {
-            if (Math.min(...xVec) < this.xRange[0]) {
-                this.xRange[0] = Math.min(...xVec);
+            if (Math.min(x1, x2) < this.xRange[0]) {
+                this.xRange[0] = Math.min(x1, x2);
             }
-            if (Math.max(...xVec) > this.xRange[1]) {
-                this.xRange[1] = Math.max(...xVec);
+            if (Math.max(x1, x2) > this.xRange[1]) {
+                this.xRange[1] = Math.max(x1, x2);
             }
-            if (Math.min(...yVec) < this.yRange[0]) {
-                this.yRange[0] = Math.min(...yVec);
+            if (Math.min(y1, y2) < this.yRange[0]) {
+                this.yRange[0] = Math.min(y1, y2);
             }
-            if (Math.max(...yVec) > this.yRange[1]) {
-                this.yRange[1] = Math.max(...yVec);
+            if (Math.max(y1, y2) > this.yRange[1]) {
+                this.yRange[1] = Math.max(y1, y2);
             }
         }
         this.generateBlockSize();
-
-        this.vectors.push([x, y, start, label]);
-        return 1;
     }
 
     this.generateBlockSize = function() {
@@ -99,8 +115,15 @@ function Graph(id) {
         this.ySize = ySize;
     }
 
-    this.realCoords = function(x, y) {
-        // TODO: normalize coords
-        
+    this.getDrawCoord = function(coord, isX = true) {
+        if (isX) {
+            let coord1 = coord - xRange[0];
+            coord1 *= this.xSize;
+        } else {
+            let coord1 = coord - yRange[0];
+            coord1 *= this.ySize;
+        }
+
+        return coord
     }
 }
